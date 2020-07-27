993ed68365759b90efb0032d857569fa83251bcd
Gagandeep Singh <deepgagan9336@gmail.com>
2020-07-27 21:15:12 +0530

    make it better

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: '/',
-    port: '3001'
+    host: 'gagan-video.herokuapp.com/',
+    port: '1222'
 })
 const myVideo = document.createElement('video')
 myVideo.muted = true
