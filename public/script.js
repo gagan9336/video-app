b66665dc9a4aafb397f26cb5f1e6a3c3ecb062b9
Gagandeep Singh <deepgagan9336@gmail.com>
2020-07-29 19:11:02 +0530

    make it better

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: 'https://gagan-video.herokuapp.com/',
+    host: 'gagan-video.herokuapp.com/',
     port: 1222,
     path: '/'
 })
